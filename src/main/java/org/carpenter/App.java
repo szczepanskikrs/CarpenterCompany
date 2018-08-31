af6192cf979ad758fd1cb0e6fc26770b3e006c12
rybsonm <ryba.marcin20@gmail.com>
2018-08-31 17:55:38 +0200

    first commit

--- src/main/java/org/carpenter/App.java
+++ src/main/java/org/carpenter/App.java
@@ -1,10 +1,23 @@
 package org.carpenter;
 
 
-public class App 
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(final String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+                    @Override
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello World");
+                    }
+                }).build();
+        server.start();
     }
 }
